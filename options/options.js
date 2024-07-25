0ccf0fbbac55fab94725b216ab84bcc42bb693ee
Crowdbotics <team@crowdbotics.com>
2024-07-25 19:36:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mmchat-48867",
+  url: "https://mmchat-48867.botics.co",
+  api: "https://mmchat-48867.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
